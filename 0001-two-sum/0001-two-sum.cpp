1699908bb5c734af4963c61e550108673ee0d3ea
Deepanshu Joshi <deepanshujoshi901@gmail.com>
2026-07-03 09:07:09 +0530

    Time: 3 ms (66.88%), Space: 14.9 MB (37.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> copy(nums);
-        sort(copy.begin(), copy.end());
-
-        int i = 0, j = copy.size() - 1;
-        int a, b;
-
-        while (i < j) {
-            int sum = copy[i] + copy[j];
-
-            if (sum < target)
-                i++;
-            else if (sum > target)
-                j--;
-            else {
-                a = copy[i];
-                b = copy[j];
-                break;
+        map<int, int> mpp;
+        for (int i = 0; i<nums.size(); i++){
+            int a = nums[i];
+            int more = target - a;
+            if (mpp.find(more) != mpp.end()){
+                return {i, mpp[more]};
             }
+            mpp[a] = i;
         }
 
-        vector<int> result;
-
-        for (int k = 0; k < nums.size(); k++) {
-            if (nums[k] == a) {
-                result.push_back(k);
-                break;
-            }
-        }
-
-        for (int k = 0; k < nums.size(); k++) {
-            if (nums[k] == b && k != result[0]) {
-                result.push_back(k);
-                break;
-            }
-        }
-
-        return result;
+        return {0,0};
     }
 };
