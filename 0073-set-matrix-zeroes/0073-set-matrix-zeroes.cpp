8083cc368c28ac0ea10777819994d9521d21dd44
Deepanshu Joshi <deepanshujoshi901@gmail.com>
2026-07-07 09:05:35 +0530

    Time: 7 ms (19.17%), Space: 21.4 MB (13.03%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& nums) {
-        vector<int> zeroRow;
         vector<int> zeroCol;
 
         for (int i = 0; i < nums.size(); i++) {
+            int found = 0;
             for (int j = 0; j < nums[0].size(); j++) {
                 if (nums[i][j] == 0) {
-                    zeroRow.push_back(i);
+                    found = 1;
                     zeroCol.push_back(j);
+                }
+            }
+            if (found == 1){
+                for (int j = 0; j< nums[0].size(); j++){
+                    nums[i][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums[0].size(); j++) {
-                if (find(zeroRow.begin(), zeroRow.end(), i) != zeroRow.end()) {
-                    nums[i][j] = 0;
-                }
-                else if (find(zeroCol.begin(), zeroCol.end(), j) != zeroCol.end()) {
+        for (int j = 0; j<nums[0].size(); j++){
+            if (find(zeroCol.begin(), zeroCol.end(), j) != zeroCol.end()){
+                for (int i = 0; i<nums.size(); i++){
                     nums[i][j] = 0;
                 }
             }
