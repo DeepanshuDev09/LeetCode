ba15d027648d4e1f7495becffc54127d2a1f62cf
Deepanshu Joshi <deepanshujoshi901@gmail.com>
2026-07-10 08:35:09 +0530

    Time: 15 ms (9.07%), Space: 27.1 MB (7.82%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int times = nums.size()/3;
+        vector<int> result;
+        map<int, int> freq;
+
+        for(int i: nums){
+            freq[i]++;
+        }
+
+        for(auto i: freq){
+            if (i.second > times) result.push_back(i.first);
+        }
+
+        return result;
+    }
+};
