25f95562dcd3128f2fb1d29ce29e6da084bf2c1b
Deepanshu Joshi <deepanshujoshi901@gmail.com>
2026-07-25 10:55:17 +0530

    Time: 75 ms (85.12%), Space: 11 MB (78.38%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -4,23 +4,20 @@ public:
         int ans = 0;
 
         for (int i = 0; i < s.size(); i++) {
-            unordered_map<char, int> freq;
-            multiset<int> st;
+            vector<int> count(26);
 
             for (int j = i; j < s.size(); j++) {
+                int curr = s[j] - 'a';
+                count[curr]++;
 
-                if (freq[s[j]] > 0) {
-                    auto it = st.find(freq[s[j]]);
-                    st.erase(it);
+                int minFreq = INT_MAX;
+                int maxFreq = INT_MIN;
+                for (int i: count){
+                    if (i != 0) minFreq = min(minFreq, i);
+                    maxFreq = max(maxFreq, i);
                 }
-
-                freq[s[j]]++;
-                st.insert(freq[s[j]]);
-
-                int minFreq = *st.begin();
-                int maxFreq = *st.rbegin();
-
-                ans += maxFreq - minFreq;
+                if (minFreq == INT_MAX) continue;
+                ans += (maxFreq - minFreq);
             }
         }
 
