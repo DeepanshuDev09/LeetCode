1bcb89043f4066bfe19236319f6c566a2c718c9b
Deepanshu Joshi <deepanshujoshi901@gmail.com>
2026-09-08 09:01:27 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.2%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        vector<int> changeInHand;
+        int countFive = 0, countTen = 0, countTwenty = 0;
 
-        for (int i = 0; i<bills.size(); i++){
-            if (bills[i] > 5){
-                int n = changeInHand.size() - 1;
-                int need = bills[i] - 5;
-                sort(changeInHand.begin(), changeInHand.end());
-
-                while (n >= 0 && need != 0){
-                    if (changeInHand[n] <= need){
-                        need -= changeInHand[n];
-                        changeInHand[n] = 0;
-                    }
-                    
-                    n--;
+        for (int i: bills){
+            if (i == 5){
+                countFive++;
+            }
+            else if (i == 10){
+                if (countFive == 0) return false;
+                countFive--;
+                countTen++;
+            }
+            else {
+                if (countFive == 0) return false;
+                if (countTen == 0){
+                    if (countFive < 3) return false;
+                    countFive -= 3;
+                }
+                else {
+                    countFive--;
+                    countTen--;
                 }
 
-                if (n < 0 && need != 0) return false;
+                countTwenty++;
             }
-            
-            changeInHand.push_back(bills[i]);
         }
 
         return true;
