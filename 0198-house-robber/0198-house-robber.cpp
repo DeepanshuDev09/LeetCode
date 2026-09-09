4305f17e5ff81c10a8c3319b7105163b054480b0
Deepanshu Joshi <deepanshujoshi901@gmail.com>
2026-09-09 20:21:27 +0530

    Time: 0 ms (100%), Space: 10.1 MB (79.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,7 +7,7 @@ public:
         if (n == 1) return nums[0];
         if (n == 2) return max(nums[0], nums[1]);
 
-        for (int i = 1; i<n; i++){
+        for (int i = 0; i<n; i++){
             int curr = nums[i];
             if (i - 3 >= 0) curr = max(nums[i]+nums[i-2], nums[i]+nums[i-3]);
             if (i - 2 >= 0) curr = max(nums[i]+nums[i-2], curr);
